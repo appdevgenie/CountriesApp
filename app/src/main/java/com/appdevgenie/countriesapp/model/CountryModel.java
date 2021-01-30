1471b625924282d7ae9181aca760fb954aac37d6
appdevgenie <appdevgenie@gmail.com>
2021-01-30 08:46:06 +0200

    add retrofit api and service

--- app/src/main/java/com/appdevgenie/countriesapp/model/CountryModel.java
+++ app/src/main/java/com/appdevgenie/countriesapp/model/CountryModel.java
@@ -1,9 +1,14 @@
 package com.appdevgenie.countriesapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CountryModel {
 
+    @SerializedName("name")
     String countryName;
+    @SerializedName("capital")
     String capital;
+    @SerializedName("flagPNG")
     String flag;
 
     public CountryModel(String countryName, String capital, String flag) {
